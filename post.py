181622780e69231b1fd16cbbd75ea23040926392
Dylan Yu <yudl.nju@gmail.com>
2013-12-13 14:26:25 +0800

    list, create, delete

--- post.py
+++ post.py
@@ -6,30 +6,48 @@ headers = {"Content-type": "application/json","Accept": "application/json"}
 response = requests.post(url, data=json.dumps(post_data), headers=headers)
 data = response.json()
 
-token_id = data['access']['token']['id']
-token_issued_at = data['access']['token']['issued_at']
-token_expires = data['access']['token']['expires']
+token_id = str(data['access']['token']['id'])
+token_issued_at = str(data['access']['token']['issued_at'])
+token_expires = str(data['access']['token']['expires'])
 #print 'Issused at:%s\nExpires:%s\nToken:%s' % (token_issued_at, token_expires, token_id)
+
 service_catalog_list = data['access']['serviceCatalog']
-nova = dict()
-neutron = dict()
 nova_url = ''
 neutron_url = ''
 for service in service_catalog_list:
-    #print service['type'], service['name']
     if service['name'] == 'nova':
         nova_url = service['endpoints'][0]['publicURL']
     if service['name'] == 'neutron':
         neutron_url = service['endpoints'][0]['publicURL']
 
-#print nova_url, neutron_url
-
-#print service_catalog_list
-url = nova_url + '/servers'
+# list
+url = neutron_url + '/v2.0/networks'
 url = url.replace('controller', '114.212.189.132')
 print url
-post_data = {'X-Auth-Token':token_id}
+headers = {"Content-type": "application/json","Accept": "application/json", 'X-Auth-Token':token_id}
+response = requests.get(url, headers=headers)
+print 'status code: ', response.status_code
+print 'reason: ', response.reason
+print response.text
+
+# create
+post_data = {
+ "network":
+  {
+    "name": "sample_network", 
+    "admin_state_up": 'true'
+  }
+}
 response = requests.post(url, data=json.dumps(post_data), headers=headers)
-#data = response.json()
-#servers = data['servers']
-#print servers
+print 'status code: ', response.status_code
+print 'reason: ', response.reason
+print response.text
+
+# delete
+"""
+url = url + '/35ca32bd-c050-401d-a5c6-55f06a5392ca'
+response = requests.delete(url, headers=headers)
+print 'status code: ', response.status_code
+print 'reason: ', response.reason
+print response.text
+"""
