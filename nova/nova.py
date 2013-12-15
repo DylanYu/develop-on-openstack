5567b41455c06c42c344b17c8cae27f9da9ec004
Dylan Yu <yudl.nju@gmail.com>
2013-12-15 20:01:53 +0800

    update nova with keystone

--- nova/nova.py
+++ nova/nova.py
@@ -4,59 +4,13 @@ import requests, json
 
 class Nova:
 
-    def __init__(self, keystone_host, keystone_port, tenant_name, username, password):
-        self.set_keystone_endpoint(keystone_host, keystone_port)
-        self.set_user(tenant_name, username, password)
-        self.authenticate()
-        self.get_info_from_auth_data()
+    def __init__(self, keystone):
+        self.keystone = keystone
+        self.public_urls = keystone.get_nova_public_urls()
+        self.create_headers()
         """treat keystone host as neutron host, typically"""
         """TODO: set neutron host"""
-        self.amend_url(keystone_host)
-
-    def set_keystone_endpoint(self, keystone_host, keystone_port):
-        self.keystone_host = keystone_host
-        self.keystone_port = keystone_port
-
-    def set_user(self, tenant_name, username, password):
-        self.tenant_name = tenant_name
-        self.username = username
-        self.password = password
-
-    def authenticate(self):
-        url = 'http://' + self.keystone_host + ':' + self.keystone_port + '/v2.0/tokens'
-        user_info ={
-            "auth":{
-                "tenantName":self.tenant_name, 
-                "passwordCredentials":{
-                    "username":self.username,
-                    "password":self.password
-                }
-            }
-        }
-        headers = {"Content-type": "application/json","Accept": "application/json"}
-        response = requests.post(url, data=json.dumps(user_info), headers=headers)
-        status_code = response.status_code
-        if status_code == 200:
-            self.auth_data = response.json()
-        else:
-            """deal with faults"""
-            print 'deal with faults'
-
-    def get_info_from_auth_data(self):
-        if not hasattr(self, 'auth_data'):
-            return
-        self.token_id = str(self.auth_data['access']['token']['id'])
-        self.headers = {
-            "Content-type": "application/json",
-            "Accept": "application/json", 
-            'X-Auth-Token':self.token_id
-        }
-        service_catalog_list = self.auth_data['access']['serviceCatalog']
-        self.public_urls = []
-        for service in service_catalog_list:
-            if service['name'] == 'nova':
-                for endpoint in service['endpoints']:
-                    self.public_urls.append(endpoint['publicURL'])
+        self.amend_url(keystone.get_keystone_host())
 
     def amend_url(self, correct_host_name):
         if not hasattr(self, 'public_urls'):
@@ -65,6 +19,14 @@ class Nova:
         for url in self.public_urls:
             urls.append(url.replace('controller', correct_host_name))
             self.public_urls = urls
+
+    def create_headers(self):
+        token_id = self.keystone.get_token_id()
+        self.headers = {
+            "Content-type": "application/json",
+            "Accept": "application/json", 
+            'X-Auth-Token':token_id
+        }
 
 
     """Server related operations"""
