62278b928a76a2ce3df627e756795e4b53745dec
Dylan Yu <yudl.nju@gmail.com>
2014-01-06 20:58:56 +0800

    init setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name='osdk',
+    version='1.0',
+    description='A Complete OenStack SDK',
+    author='Dylan Yu',
+    packages=setuptools.find_packages())
