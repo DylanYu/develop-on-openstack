ae0ca1a1ef0b4255607950654e23494567e12005
Dongliang Yu <yudl.nju@gmail.com>
2014-05-16 22:14:22 +0800

    refactor

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ import setuptools
 setuptools.setup(
     name='osdk',
     version='1.0',
-    description='A Complete OenStack SDK',
-    author='Dylan Yu',
+    description='A Complete OpenStack SDK',
+    author='Dongliang Yu',
     packages=setuptools.find_packages())
